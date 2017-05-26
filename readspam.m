15ed115ce10dad2fff704a2fe812263e7e8f3bc2
Kevin Tham <kevintham91@gmail.com>
2017-05-26 16:57:02 +0800

    Tidied up readspam.m, used textscan

--- readspam.m
+++ readspam.m
@@ -6,64 +6,38 @@ clear ; close all; clc
 % path to spamassassin dataset:
 path = '/home/kevintham/datasets/spamassassin/';
 
-spam = strcat(path, 'spam/.');
-spam_2 = strcat(path, 'spam_2/.');
-easy_ham = strcat(path, 'easy_ham/.');
-easy_ham_2 = strcat(path, 'easy_ham_2/.');
-hard_ham = strcat(path, 'hard_ham/.');
+spam = strcat(path, 'spam/');
+spam_2 = strcat(path, 'spam_2/');
+easy_ham = strcat(path, 'easy_ham/');
+easy_ham_2 = strcat(path, 'easy_ham_2/');
+hard_ham = strcat(path, 'hard_ham/');
 
-dinfo1 = dir(spam)(3:end-1);
-dinfo2 = dir(spam_2)(3:end-1);
-dinfo3 = dir(easy_ham)(3:end-1);
-dinfo4 = dir(easy_ham_2)(3:end-1);
-dinfo5 = dir(hard_ham)(3:end-1);
+folders = {spam, spam_2, easy_ham, easy_ham_2, hard_ham};
+lengths = [500, 1396, 2500, 1400, 250];
+m = sum(lengths);
 
-l1 = size(not([dinfo1.isdir]))(2);
-l2 = size(not([dinfo2.isdir]))(2);
-l3 = size(not([dinfo3.isdir]))(2);
-l4 = size(not([dinfo4.isdir]))(2);
-l5 = size(not([dinfo5.isdir]))(2);
-l_all = l1+l2+l3+l4+l5;
-
-y = cat(1, zeros(l1+l2,1), ones(l3+l4+l5,1));
+y = cat(1, zeros(lengths(1)+lengths(2),1), ones(lengths(3)+lengths(4) ...
+    +lengths(5),1));
 
 data_cell = {};
 
-for i = 1:l1
-  file = dinfo1(i).name;
-  mail = strjoin(textread(strcat(spam(1:end-1),file), '%s'));
-  proc_mail = preproc(mail);
-  data_cell{i} = proc_mail;
+for j = 1:5
+  dinfo = dir(folders{j})(3:end-1);
+  count = 0;
+  for i = 1:lengths(j)
+    file = dinfo(i).name;
+    filepath = strcat(folders{j}, file);
+    fprintf('Preprocessing current file: %s \n', filepath);
+    if exist('OCTAVE_VERSION')
+       fflush(stdout);
+    end
+    fid = fopen(filepath);
+    mail = strjoin(textscan(fid, '%s'){1,1});
+    proc_mail = preproc(mail);
+    data_cell{i + count} = proc_mail;
+  end
+  count = count + lengths(j);
 end
-
-for i = 1:l2
-  file = dinfo2(i).name;
-  mail = strjoin(textread(strcat(spam_2(1:end-1), file),'%s'));
-  proc_mail = preproc(mail);
-  data_cell{i+l1} = proc_mail;
-end
-
-for i = 1:l3
-  file = dinfo3(i).name;
-  mail = strjoin(textread(strcat(easy_ham(1:end-1), file),'%s'));
-  proc_mail = preproc(mail);
-  data_cell{i+l1+l2} = proc_mail;
-end
-
-for i = 1:l4
-  file = dinfo4(i).name;
-  mail = strjoin(textread(strcat(easy_ham_2(1:end-1), file),'%s'));
-  proc_mail = preproc(mail);
-  data_cell{i+l1+l2+l3} = proc_mail;
-end
-
-for i = 1:l5
-  file = dinfo5(i).name;
-  mail = strjoin(textread(strcat(hard_ham(1:end-1), file),'%s'));
-  proc_mail = preproc(mail);
-  data_cell{i+l1+l2+l3+l4} = proc_mail;
-end
-
 
 % assemble vocab list
 split_data = textscan(strjoin(data_cell), '%s'){1,1};
